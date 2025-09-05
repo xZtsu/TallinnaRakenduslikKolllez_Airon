3df5f8785790e2a00828a9da860732f0ce2f6553
Opilane TTHK <airon.tatrik@gmail.com>
2025-09-05 11:38:05 +0300

    a

---
+++ TallinnaRakenduslikKolllež/Controllers/StudentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TallinnaRakenduslikKolllež.Data;
+
+namespace TallinnaRakenduslikKolllež.Controllers
+{
+    public class StudentsController : Controller
+    {
+       
+            private readonly SchoolContext _context;
+            public StudentsController(SchoolContext context)
+            {
+                _context = context;
+            }
+        
+        
+            public async Task<IActionResult> Index()
+            {
+                return View(await _context.Students.ToListAsync());
+            }
+    }
+}
