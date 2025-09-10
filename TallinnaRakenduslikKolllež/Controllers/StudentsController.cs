c981225ef6f09f4e73fc5072672a0a5f8f9d8564
Opilane TTHK <opilane@tthk.ee>
2025-09-10 13:12:42 +0300

    Delete Students func added

--- TallinnaRakenduslikKolllež/Controllers/StudentsController.cs
+++ TallinnaRakenduslikKolllež/Controllers/StudentsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TallinnaRakenduslikKolllež.Data;
 
@@ -17,6 +18,29 @@ namespace TallinnaRakenduslikKolllež.Controllers
             public async Task<IActionResult> Index()
             {
                 return View(await _context.Students.ToListAsync());
+                        }
+        public async Task<IActionResult> Delete(int? id)
+            {
+                if(id == null)
+                {
+                    return NotFound();
+                }
+                var student = await _context.Students.SingleOrDefaultAsync(m => m.Id == id);
+                if(student == null)
+                {
+                    return NotFound();
+                }
+                   return View(student);
+            }
+            [HttpPost, ActionName("Delete")]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> DeleteConfirmed(int id)
+            {
+                var student = await _context.Students.FindAsync(id);
+                _context.Students.Remove(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+
             }
     }
 }
