e187f94c322d79ad4e8f614e681a1077a11ac549
Opilane TTHK <opilane@tthk.ee>
2025-09-11 13:31:47 +0300

    Details + controllers
    Details ül

--- TallinnaRakenduslikKolllež/Controllers/StudentsController.cs
+++ TallinnaRakenduslikKolllež/Controllers/StudentsController.cs
@@ -1,46 +1,81 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
+﻿﻿
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TallinnaRakenduslikKolllež.Data;
+using TallinnaRakenduslikKolllež.Models;
 
 namespace TallinnaRakenduslikKolllež.Controllers
 {
     public class StudentsController : Controller
     {
-       
-            private readonly SchoolContext _context;
-            public StudentsController(SchoolContext context)
+        private readonly SchoolContext _context;
+        public StudentsController(SchoolContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Students.ToListAsync());
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id, LastName, FirstName, EnrollmentDate")] Student student)
+        {
+            if (ModelState.IsValid)
             {
-                _context = context;
-            }
-        
-        
-            public async Task<IActionResult> Index()
-            {
-                return View(await _context.Students.ToListAsync());
-                        }
-        public async Task<IActionResult> Delete(int? id)
-            {
-                if(id == null)
-                {
-                    return NotFound();
-                }
-                var student = await _context.Students.SingleOrDefaultAsync(m => m.Id == id);
-                if(student == null)
-                {
-                    return NotFound();
-                }
-                   return View(student);
-            }
-            [HttpPost, ActionName("Delete")]
-            [ValidateAntiForgeryToken]
-            public async Task<IActionResult> DeleteConfirmed(int id)
-            {
-                var student = await _context.Students.FindAsync(id);
-                _context.Students.Remove(student);
+                _context.Students.Add(student);
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Index");
-
+                // return RedirectToAction(nameof(Index))
             }
+            return View(student);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.Students.FirstOrDefaultAsync(m => m.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.Students.FirstOrDefaultAsync(m => m.Id == id);
+            if (student == null) 
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
     }
 }
